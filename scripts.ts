34c20675f9bd5410b5d76b213083cb43d66db7e0
Angel Chávez <infoangelchavez@gmail.com>
2024-04-15 18:05:53 -0500

    mod- Password length: 60

--- scripts.ts
+++ scripts.ts
@@ -14,7 +14,7 @@ const opendb = async () => {
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name VARCHAR(50) NOT NULL,
 email VARCHAR(256) UNIQUE NOT NULL,
-password VARCHAR(60) NOT NULL,
+password CHARACTER(60) NOT NULL,
 confirm BOOLEAN NOT NULL,
 token CHARACTER(15) UNIQUE);`);
   db.close();
